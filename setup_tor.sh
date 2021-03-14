e4f9e9a49b557e010848d1a361fdbb1c5ce9c32f
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2021-03-14 17:35:21 +0100

    setup tor fixes

--- setup_tor.sh
+++ setup_tor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+TOR_CONFIG=/usr/local/etc/tor/torrc
+
 tor_password="scholarly_password"
 hashed_password=$(tor --hash-password $tor_password)
-echo "ControlPort 9051" | sudo tee /etc/tor/torrc
-echo "HashedControlPassword $hashed_password" | sudo tee -a /etc/tor/torrc
+echo "ControlPort 9051" | tee $TOR_CONFIG
+echo "HashedControlPassword $hashed_password" | tee -a $TOR_CONFIG
 
 #sudo service tor stop
 #sudo service tor start
